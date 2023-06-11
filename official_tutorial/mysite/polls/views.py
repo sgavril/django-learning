1bc031d08f97c9a43cdf95d0905b71279ee5b146
--replace-all <stefan.gavriliuc@ucalgary.ca>
2023-06-11 16:14:08 -0600

    fix: amended views to use generic views

--- official_tutorial/mysite/polls/views.py
+++ official_tutorial/mysite/polls/views.py
@@ -1,9 +1,30 @@
-from django.http import Http404, HttpResponse
+from typing import Any
+from django.db.models.query import QuerySet
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from .models import Question
+from django.urls import reverse
+from django.views import generic
 
+from .models import Choice, Question
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self) -> QuerySet[Any]:
+        """ Return the last 5 published questions. """
+        return Question().objects.order_by("-pub_date")[:5]
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+"""
+ Old, simple way
 def index(request):
-    """ Display the latest 5 poll questions in the system. """
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
@@ -15,6 +36,23 @@ def detail(request, question_id):
 def results(request, question_id):
     response = "You are looking at the results of question %s."
     return HttpResponse(response % question_id)
+"""
 
 def vote(request, question_id):
-    return HttpResponse("You are voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Re-display the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))

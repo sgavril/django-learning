0784db425b66009f24b5407151a13d3467d30d06
--replace-all <stefan.gavriliuc@ucalgary.ca>
2023-06-26 07:18:52 -0600

    added simple f.b.v.s for sprinkles

--- twoscoops/icecreamratings/sprinkles/utils.py
+++ twoscoops/icecreamratings/sprinkles/utils.py
@@ -3,6 +3,8 @@ from django.http import HttpRequest
 
 def check_sprinkle_rights(request: HttpRequest) -> HttpRequest:
     if request.user.can_sprinkle or request_.user.is_staff:
+        # Python is dynamically typed ; can add attributes to request
+        request.can_sprinkle
         return request
 
     raise PermissionDenied
--- twoscoops/icecreamratings/sprinkles/views.py
+++ twoscoops/icecreamratings/sprinkles/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or404
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+from .models import Sprinkle
+from .utils import check_sprinkle_rights
+
+
+def sprinkle_list(request: HttpRequest) -> HttpResponse:
+    """Standard list view"""
+    request = check_sprinkles(request)
+
+    return render(request,
+    "sprinkles/sprinkle_list.html",
+    {"sprinkles": Sprinkle.objects.all()})
+
+def sprinkle_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    """Standard detail view."""
+    request = check_sprinkles(request)
+
+    sprinkle = get_object_or404(Sprinkle, pk=pk)
+
+    return render(request, "sprinkles/sprinkle_detail.html",
+        {"sprinkle": sprinkle})
+
+def sprinkle_preview(request: HttpRequest) -> HttpResponse:
+    """ Preview of new sprinkle, but without check_sprinkles """
+    sprinkle = Sprinkle.objects.all()
+    return render(request, "sprinkles/sprinkle_preview.html")

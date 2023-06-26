2aac9fd5f479ebdab08a672ccd85f57a7b1ed2d3
--replace-all <stefan.gavriliuc@ucalgary.ca>
2023-06-25 23:45:19 -0600

    made polls app + most basic view

--- official_tutorial/mysite/mysite/urls.py
+++ official_tutorial/mysite/mysite/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
+    path("posts/", include("posts.urls")),
     path("admin/", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
 ]

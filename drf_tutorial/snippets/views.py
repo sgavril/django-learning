f2d8e230aef347852bdd94a1f9118b4587b3a14c
--replace-all <stefan.gavriliuc@ucalgary.ca>
2023-06-29 23:28:02 -0600

    refactor: condensed mixins

--- drf_tutorial/snippets/views.py
+++ drf_tutorial/snippets/views.py
@@ -1,36 +1,12 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-from rest_framework import mixins, generics
+from rest_framework import generics
 
 
-class SnippetList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    # Methods provided by mixins are not tied to particular HTTP method
-    # So we add the following methods
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class SnippetDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-

2688adf1c461d6bcd60e15c3f4f032fbfa2afdd8
--replace-all <stefan.gavriliuc@ucalgary.ca>
2023-06-30 22:50:30 -0600

    feat: added User views

--- drf_tutorial/snippets/views.py
+++ drf_tutorial/snippets/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth.models import User
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
 
 
@@ -7,6 +8,20 @@ class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+    # Override perform_create to modify how instance is saved
+    # allowing the association of snippets with users
+    def perform_create(self, serializer):
+        serializer.save(owner.self.request.user)
+
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+class UserList(generics.ListAPIView):
+    """ Read-only user class-based views. """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer()
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
     serializer_class = SnippetSerializer

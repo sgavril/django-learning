c5478be3c68daa4367b7532f91236eba7baac5ec
--replace-all <stefan.gavriliuc@ucalgary.ca>
2023-07-03 23:37:38 -0600

    refactor: use routers

--- drf_tutorial/snippets/urls.py
+++ drf_tutorial/snippets/urls.py
@@ -1,20 +1,12 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
-from .views import *
+
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet, basename='snippet')
+router.register(r'users', views.UserViewSet, basename='user')
 
 urlpatterns = [
-    path('', views.api_root),
-    path('api/', views.api_root, name='api_root'),
-    path('snippets/', snippet_list, name='snippet-list'),
-    path('snippets/<int:pk>', snippet_detail, name='snippet-detail'),
-    path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
-    path('users/', views.user_list, name='user-list'),
-    path('users/<int:pk>/', user_detail, name='user-detail')
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
-
-urlpatterns += [
-    path('api-auth/', include('rest_framework.urls')),
+    path('', include(router.urls)),
 ]

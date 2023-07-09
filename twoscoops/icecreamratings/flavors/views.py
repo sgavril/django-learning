c83be041698677ac16721c1e35d31b92c21bf4d0
--replace-all <stefan.gavriliuc@ucalgary.ca>
2023-07-09 16:21:09 -0600

    feat: add flavor view with django generic View

--- twoscoops/icecreamratings/flavors/views.py
+++ twoscoops/icecreamratings/flavors/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.utils.functional import cached_property
-from django.views.generic import CreateView, UpdateView, TemplateView, ListView
+from django.views.generic import CreateView, UpdateView, TemplateView, ListView, View
+from django.shortcuts import get_object_or_404, render, redirect
 
 from .models import Flavor
 from .mixins import FreshFruitMixin, FavoriteMixin, FlavorActionMixin
@@ -40,3 +41,21 @@ class FlavorListView(ListView):
             return queryset.filter(title__icontains=q)
 
         return queryset
+
+def flavor_view(request):
+    return render(request, 'flavours/base.html')
+
+class FlavorView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        flavor = get_object_or_404(Flavor, slug=kwargs['slug'])
+        return render(request,
+            "flavors/flavor_detail.html",
+            {"flavour": flavor}
+        )
+
+    def post(self, request, *args, **kwargs):
+        flavor = get_object_or_404(Flavor, slug=kwargs['slug'])
+        form = FlavorForm(request.POST, instance=flavor)
+        if form.is_valid():
+            form.save()
+        return redirect("flavors:detail", flavor.slug)

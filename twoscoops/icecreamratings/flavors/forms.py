4b7ad2a1c8c13af21ccf5b6d6efdf36fc2ff10bf
--replace-all <stefan.gavriliuc@ucalgary.ca>
2023-07-11 22:25:34 -0600

    feat: custom clean methods for forms

--- twoscoops/icecreamratings/flavors/forms.py
+++ twoscoops/icecreamratings/flavors/forms.py
@@ -11,3 +11,33 @@ class FlavorForm(forms.ModelForm):
 
     class Meta:
         model = Flavor
+
+class IceCreamOrderForm(forms.Form):
+    slug = forms.ChoiceField(label='Flavor')
+    toppings = forms.CharField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['slug'].choices = [
+            (x.slug, x.title) for x in Flavor.objects.all()
+        ]
+
+    def clean_slug(self):
+        slug = self.cleaned_data['slug']
+        if Flavor.objects.get(slug=slug).scoops_remaining <= 0:
+            msg = 'Sorry, we are out of that flavor.'
+            raise forms.ValidationError(msg)
+        return slug
+
+    def clean(self):
+        cleaned_data = super().clean()
+        slug = cleaned_data.get('slug', '')
+        toppings = cleaned_data.get('toppings', '')
+
+        # Too much chocolate validation example
+        in_slug = 'chocolate' in slug.lower()
+        in_toppings = 'chocolate' in toppings.lower()
+        if in_slug and in_toppings:
+            msg = 'Your order has too much chocolate.'
+            raise forms.ValidationError(msg)
+        return cleaned_data

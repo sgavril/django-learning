db840c34f0a13d31e0d21b43b22598368c92ce5d
--replace-all <stefan.gavriliuc@ucalgary.ca>
2023-07-11 22:35:01 -0600

    feat: add 2 views for icecreamstores

--- twoscoops/icecreamratings/store/views.py
+++ twoscoops/icecreamratings/store/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .forms import IceCreamStoreCreateForm, IceCreamStoreUpdateForm
+from .models import IceCreamStore
 
-# Create your views here.
+class IceCreamCreateView(CreateView):
+    model = IceCreamStore
+    form_class = IceCreamStoreCreateForm
+
+class IceCreamUpdateView(UpdateView):
+    model = IceCreamStore
+    form_class = IceCreamStoreUpdateForm()

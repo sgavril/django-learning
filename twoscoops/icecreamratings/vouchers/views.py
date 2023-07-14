85c8e1a4ed161a1230144107de579d29ebdfbe48
--replace-all <stefan.gavriliuc@ucalgary.ca>
2023-07-13 22:45:24 -0600

    feat: use template to display already-filtered data

--- twoscoops/icecreamratings/vouchers/views.py
+++ twoscoops/icecreamratings/vouchers/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from .models import Voucher
+
+class GreenfeldRoyView(TemplateView):
+    template_name = 'vouchers/views_conditional.html'
+
+    def get_context_data(self, **kwargs):
+        contect = super().get_context_data(**kwargs)
+        context['greenfelds'] = \
+            Voucher.objects.filter(name__icontains='greenfeld')
+        context['roys'] = \
+            Voucher.objects.filter(name__icontains='roy')
+        return context
